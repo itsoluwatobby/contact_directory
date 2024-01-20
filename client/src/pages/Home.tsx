6c8b96427a72100f1d786570d164baf1e260e8a7
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-01-20 10:33:04 +0100

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
   return (
     <main className="home flex flex-col w-full h-full">
       <Navbar />
-      <div className="relative flex w-full flex-col justify-around -mt-10 h-full">
+      <div className="relative flex w-full flex-col justify-around h-full">
         <div className="header px-5 py-2 font-mono">
           <p className="first-letter:text-6xl capitalize text-3xl w-full">Welcome to Contact Directory, Your Very <span className="text-gray-200 fonnt-bold">First Choice</span> for managing your contacts</p>
         </div>
@@ -60,7 +60,7 @@ export default function Home() {
             : setCanProceed(prev => ({...prev, openForm: true}))
           }}
           className={`w-full lg:w-[80%] text-lg font-medium py-4 focus:outline-0 bg-gradient-to-tr from-slate-700 rounded-3xl hover:opacity-90 active:opacity-100 transition-opacity`}>
-            Get Started
+            {canProceed.proceed ? 'Continue' : 'Get Started'}
           </button>
         </div>
 
