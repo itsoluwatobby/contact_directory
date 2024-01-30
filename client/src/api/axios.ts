95d5fb0ec12e66396eeb8e70e1b6ca07529f7492
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-01-30 05:21:25 +0100

    Update axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -20,7 +20,7 @@ type UserResponseType = {
     data: UserObjType
   } 
 }
-export const BASEURL = 'http://localhost:4500/api/v1'
+export const BASEURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4500/api/v1'
 
 export const contactApi = axios.create({
   baseURL: BASEURL,
